54ee1bf343a95d5479cf527291cf98278ac1e5ca
Benson Sanga <bensonsanga@hotmail.com>
2019-03-04 20:10:19 +0100

    Little stuff

--- mapping.py
+++ mapping.py
@@ -2,62 +2,62 @@ import folium
 import os
 import json
 
-#create a map
+# create a map
 
-m = folium.Map(location=[41.033965,29.228986], zoom_start=12)
+m = folium.Map(location=[51.800420, 19.751590], zoom_start=14)
 
 # Global tooltip
-tooltip = 'BensonAD Headquarters'
+tooltip = 'Click for more info'
 
-#creating custom marker icon
-logoicon = folium.features.CustomIcon('icon.png', icon_size = (30, 30))
+# creating custom marker icon
+logoicon = folium.features.CustomIcon('icon.png', icon_size=(30, 30))
 
-#Vega dat import for plotting 
+# Vega dat import for plotting
 vis = os.path.join('Data', 'vis.json')
 
-#Geojson data  import for plotting 
+# Geojson data  import for plotting
 overlay = os.path.join('Data', 'overlay.json')
 
 # Create a marker
-folium.Marker([41.033965,29.228986], 
-                popup='<strong>BensonAD Offices</strong>',
-                tooltip=tooltip).add_to(m),
-#changing icon
-folium.Marker([41.011997,29.155631], 
-                popup='<strong>BensonAD Servers</strong>',
-                tooltip=tooltip,
-                icon=folium.Icon(icon='cloud')).add_to(m), 
-#changing color
-folium.Marker([41.003707,29.192008], 
-                popup='<strong>BensonAD Secondary servers</strong>',
-                tooltip=tooltip,
-                icon=folium.Icon(color='green', icon='leaf')).add_to(m),                  
-#changing color and icon, you can use material icons
-folium.Marker([41.008111,29.218776], 
-                popup='<strong>BensonAD Waste facilities</strong>',
-                tooltip=tooltip,
-                icon=folium.Icon(color='purple')).add_to(m), 
-#with custom logo
-folium.Marker([41.015883,29.234219],
+folium.Marker([51.800457, 19.751624],
+              popup='<strong>Main Square</strong>',
+              tooltip=tooltip).add_to(m),
+# changing icon
+folium.Marker([51.801496, 19.741710],
+              popup='<strong>Old catholic church</strong>',
+              tooltip=tooltip,
+              icon=folium.Icon(icon='cloud')).add_to(m),
+# changing color
+folium.Marker([51.800442, 19.741431],
+              popup='<strong>Green park and Pond</strong>',
+              tooltip=tooltip,
+              icon=folium.Icon(color='green', icon='leaf')).add_to(m),
+# changing color and icon, you can use material icons
+folium.Marker([51.800442, 19.741231],
+              popup='<strong>BensonAD Waste facilities</strong>',
+              tooltip=tooltip,
+              icon=folium.Icon(color='purple')).add_to(m),
+# with custom logo
+folium.Marker([51.800442, 19.741331],
               popup='<strong>BensonAD master</strong>',
               tooltip=tooltip,
               icon=logoicon).add_to(m),
 
-#New vega marker
-folium.Marker([41.009241,29.237917], 
-                popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(vis)), width=450,height=250))).add_to(m), 
+# New vega marker
+folium.Marker([51.800442, 19.741531],
+              popup=folium.Popup(max_width=450).add_child(folium.Vega(json.load(open(vis)), width=450, height=250))).add_to(m),
 
-#geojson overlay
+# geojson overlay
 folium.GeoJson(overlay, name='cambridge').add_to(m)
 
-#generate circle marker for areas
+# generate circle marker for areas
 folium.CircleMarker(
-    location=[41.021412,29.219159],
+    location=[51.800457, 19.751624],
     radius=50,
     popup="My site area",
     color='#428bca',
-    fill= True,
-    fill_color = '#428bca'
+    fill=True,
+    fill_color='#428bca'
 ).add_to(m),
 # Generate Map
 m.save('map.html')
--- matokeo.py
+++ matokeo.py
@@ -7,15 +7,25 @@ import pandas as pd
 print("Welcome to your results")
 
 iNumber = input("Please enter your student number e.g S0189/0001")
+iYear = input("Enter year")
 iSchoolnumber = iNumber[:5].lower()
-iLink = "https://matokeo.necta.go.tz/csee/results/{}.htm".format(iSchoolnumber)
+iLink = "https://maktaba.tetea.org/exam-results/{exam}{year}/{number}.htm".format(year=iYear, exam='CSEE', number=iSchoolnumber)
+if (request = requests.get(iLink)):
+      iLink = iLink
+else:
+    iLink = "https://maktaba.tetea.org/exam-results/{exam}{year}/{number}.htm".format(year=iYear, exam='CSEE', number=iSchoolnumber.upper())  
 
+print(iLink)
 
+'''
 #Getting results from pages
 request = requests.get(iLink)
 content = request.content
 soup = BeautifulSoup(content, "html.parser")
-student = soup.find("font", string=iNumber.upper())
+if (soup.find("font", string=iNumber.upper())):
+      student = soup.find("font", string=iNumber.upper())
+else:
+      student = soup.find("font", string=iNumber[6:10].upper())
 studentReport = student.parent.parent
 studentResults = studentReport.find_all("font")
 studentNumber = studentResults[0].text
@@ -25,10 +35,11 @@ studentDivision = studentResults[3].text
 studentGrades = studentResults[4].text
 
 
-print("Your number is ",
-      studentNumber,". Your have got Division ",
-      studentDivision," by ",
-      studentDivisionPoint,
-      ". And your grades are ",
-      studentGrades)
+
+#print("Your number is ",
+ #    studentDivision," by ",
+  #    studentDivisionPoint,
+   #   ". And your grades are ",
+    #  studentGrades)
+    '''
 
