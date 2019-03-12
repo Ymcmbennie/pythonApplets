4aa18cf81708157ed110c5db059c49ba55dff048
Benson Sanga <bensonsanga@hotmail.com>
2019-03-12 01:11:00 +0100

    Link generator

---
+++ Link automator.py
@@ -0,0 +1,3 @@
+for i in range(22):
+      link = '<li><a href="/static/resources/svgs_collection (%s).zip" download>svgs_collection-%s</a></li>'%(i,i)
+      print (link)
--- flaskapp/application.py
+++ flaskapp/application.py
@@ -1,11 +1,20 @@
-from flask import Flask
+import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Benson right"
+    return render_template('index.html', name="Benson Sanga")
 
 @app.route("/dilara")
 def dilara():
-    return "Benson found Dilara"
+    return render_template('index.html', name=datetime.datetime.now())
+
+@app.route("/forloop")
+def forloop():
+    names=["benson", "dilara", "beril", "gizem"]
+    return render_template('index02.html', names=names)
+
+if __name__ == '__main__':
+    app.run(debug=True)
